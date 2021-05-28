faddd2b56ad437fced2805496d269ae114ae37af
Shakiba448 <shakiba448@gmail.com>
2021-05-29 04:04:11 +0600

    WIP on next js

---
+++ pages/news.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const news = () => {
+  return (
+    <div>
+      <h1>This is news Page</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default news;
